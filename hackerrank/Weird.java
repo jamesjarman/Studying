54c1fd9a70689e4521064a7774961d1cad459bc5
jamesjarman <50597912+jamesjarman@users.noreply.github.com>
2020-09-20 18:26:16 +0100

    Add files via upload

---
+++ hackerrank/Weird.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Weird {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+
+        if (N==3) {
+            System.out.println("Weird");
+        }
+    }
+}
