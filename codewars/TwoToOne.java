e840595749cf61402d000ba8633ebf1ab5b225a2
jamesjarman <50597912+jamesjarman@users.noreply.github.com>
2020-09-21 22:08:13 +0100

    Add files via upload

---
+++ codewars/TwoToOne.java
@@ -0,0 +1,33 @@
+package codewars;
+
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.junit.Assert.assertEquals;
+
+public class TwoToOne {
+
+    public static String longest (String s1, String s2) {
+
+
+        if (isNull(s1) || isNull(s2)) {
+            return "";
+        }
+
+        return s1.concat(s2).chars().distinct().mapToObj(c -> String.valueOf((char)c)).sorted().collect(Collectors.joining());
+
+
+
+     }
+
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals("", TwoToOne.longest("", null));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest("inmanylanguages", "theresapairoffunctions"));
+    }
+}
