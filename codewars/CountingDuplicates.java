62df1737c6a7698d7c20c8168eaa2e5466011ce3
jamesjarman <50597912+jamesjarman@users.noreply.github.com>
2020-09-28 19:43:34 +0100

    Add files via upload

---
+++ codewars/CountingDuplicates.java
@@ -0,0 +1,52 @@
+package codewars;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        Set<String> count = new HashSet<>();
+
+        for (String string : text.toLowerCase().split("")) {
+            if (text.toLowerCase().indexOf(string) != text.toLowerCase().lastIndexOf(string)) count.add(string);
+        }
+
+        return count.size();
+    }
+
+
+
+    @Test
+    public void abcdeReturnsZero() {
+        assertEquals(0, CountingDuplicates.duplicateCount("abcde"));
+    }
+
+    @Test
+    public void abcdeaReturnsOne() {
+        assertEquals(1, CountingDuplicates.duplicateCount("abcdea"));
+    }
+
+    @Test
+    public void indivisibilityReturnsOne() {
+        assertEquals(1, CountingDuplicates.duplicateCount("indivisibility"));
+    }
+
+    @Test
+    public void reallyLongStringContainingDuplicatesReturnsThree() {
+        String testThousandA = new String(new char[1000]).replace('\0', 'a');
+        String testHundredB = new String(new char[100]).replace('\0', 'b');
+        String testTenC = new String(new char[10]).replace('\0', 'c');
+        String test1CapitalA = new String(new char[1]).replace('\0', 'A');
+        String test1d = new String(new char[1]).replace('\0', 'd');
+        String test = test1d + test1CapitalA + testTenC +
+                testHundredB + testThousandA;
+
+
+        assertEquals(3, CountingDuplicates.duplicateCount(test));
+    }
+}
